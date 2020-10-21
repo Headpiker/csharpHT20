38ca69b23b4e8c86a90c09102f659778a1ee89c0
matildawe <63040887+matildawe@users.noreply.github.com>
2020-10-21 11:41:03 +0200

    Addinf DAL, BL and models

---
+++ Models/Podcast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Models
+{
+    public class Podcast
+    {
+        public Category Category { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+
+        public Podcast(string title, string description, string url)
+        {
+
+        }
+    }
+}
