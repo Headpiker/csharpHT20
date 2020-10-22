4c4ada3dc68ee0ce3025c313d553637a230840d3
jenniifer99 <63098327+jenniifer99@users.noreply.github.com>
2020-10-22 15:43:56 +0200

    Category BL, DAL

---
+++ DAL/DataManagerCategory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace DAL
+{
+    class DataManagerCategory
+    {
+        public void Serialize(List<Category> categories)
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(categories.GetType());
+                using (FileStream outFile = new FileStream("Categories.xml", FileMode.Create, FileAccess.Write))
+                {
+                    xmlSerializer.Serialize(outFile, categories);
+                } 
+            }
+            catch (Exception)
+            {
+                throw new SerializerException("Categories.xml", "Could not serialize!");
+            }
+        }
+
+        public List<Category> Deserialize()
+        {
+            try
+            {
+                List<Category> categoryListToBeReturned;
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Category>));
+                using (FileStream inFile = new FileStream("Categories.xml", FileMode.Open, FileAccess.Read))
+                {
+                    categoryListToBeReturned = (List<Category>)xmlSerializer.Deserialize(inFile);
+                }
+                return categoryListToBeReturned;
+            }
+            catch (Exception)
+            {
+                throw new SerializerException("Categories.xml", "Could not deserialize!");
+            }
+        }
+    }
+}
