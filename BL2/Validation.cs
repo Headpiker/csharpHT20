8f9732b1e2ff6fa272292df1575fbbd4da725fff
headpiker <headpiker@gmail.com>
2020-10-26 16:53:03 +0100

    Validering (URL, duplicate, fileExisting, field null/whitespace)

--- BL2/Validation.cs
+++ BL2/Validation.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
 
 namespace BL
 {
@@ -10,15 +12,28 @@ namespace BL
     {
         public static bool IsUrlValid(string url)
         {
-            if (url.StartsWith("http://") || url.StartsWith("https://"))
-            {
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("URL har inte korrekt format");
-                return false;
-            }
+            return url.StartsWith("https://") || url.StartsWith("http://") ? true : false;
+        }
+
+        public static bool IsDuplicate(string newContent, string[] content) 
+        {
+            //Jämför inputs och kollar om det är samma, oavsett om det är små eller stora tecken
+            return content.Any((i) => String.Equals(i, newContent, StringComparison.OrdinalIgnoreCase)); 
+        }
+        
+        public static bool IsFileExisting (string filePath)
+        {
+            return File.Exists(filePath);
+        }
+
+        public static bool IsFieldNullOrEmpty (string emptyField)
+        {
+            return String.IsNullOrEmpty(emptyField) ? true : false;
+        }
+
+        public static bool IsFieldNullOrWhitespace(string emptyWhitespace)
+        {
+            return String.IsNullOrWhiteSpace(emptyWhitespace) ? true : false;
         }
     }
 }
