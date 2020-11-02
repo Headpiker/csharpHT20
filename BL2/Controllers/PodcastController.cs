31796ded192758a2d150caf0e21f85a8b29bf75b
Headpiker <50620285+Headpiker@users.noreply.github.com>
2020-11-02 12:36:23 +0100

    Exceptions i form och podcastcontroller

--- BL2/Controllers/PodcastController.cs
+++ BL2/Controllers/PodcastController.cs
@@ -24,12 +24,17 @@ namespace BL.Controllers
         }
         public async void CreatePodcastObject(string title, string url, string category, int updateInterval)
         {
-            if (Validation.IsUrlValid(url))
+            try
             {
-                List<Episode> episodes = await episodeRepository.GetEpisodesFromRSS(url);
-                Podcast podcast = new Podcast(title, url, category, updateInterval, episodes);
-                podcastRepository.Create(podcast);
+
+                if (Validation.IsUrlValid(url))
+                {
+                    List<Episode> episodes = await episodeRepository.GetEpisodesFromRSS(url);
+                    Podcast podcast = new Podcast(title, url, category, updateInterval, episodes);
+                    podcastRepository.Create(podcast);
+                }
             }
+            catch (Exception ex) { MessageBox.Show("Något gick fel, pröva igen!" + "\n" + ex.Message);}
         }
 
         public List<Podcast> GetAllPodcasts()
